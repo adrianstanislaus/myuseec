0fc92c46da9342ab28b58f19d054a614c17c6c43
adrianstanislaus <18118036@telecom.stei.itb.ac.id>
2021-09-16 00:40:00 +0700

    add featureA1

--- main.go
+++ main.go
@@ -6,4 +6,5 @@ func main() {
 	fmt.Println("This is the MAIN code")
 	fmt.Println("This is the DEVELOPMENT code")
 	fmt.Println("This is the featureA code")
+	fmt.Println("This is another featureA code")
 }
