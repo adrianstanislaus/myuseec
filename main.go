76a738c9ff3ef4988af19a013aa1f4b8436658c0
adrianstanislaus <18118036@telecom.stei.itb.ac.id>
2021-09-15 21:51:42 +0700

    reset to first condition

---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("This is the main code, the one and only, the original code")
+	fmt.Println("ini tulisan setelah stash")
+	fmt.Println("mau nyoba stash")
+}
