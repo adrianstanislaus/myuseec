07f7045d9918cc9e32257acc6e7af01df2503f74
adrianstanislaus <18118036@telecom.stei.itb.ac.id>
2021-09-15 22:22:56 +0700

    wadigidaw

--- main.go
+++ main.go
@@ -9,4 +9,7 @@ func main() {
 	fmt.Println("coding di development")
 	fmt.Println("penasaran sama stash")
 
+	fmt.Println("This is the main code, the one and only, the original code")
+	fmt.Println("ini tulisan setelah stash")
+
 }
