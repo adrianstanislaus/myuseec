6efda389fc2ed45484a591f605dc4e06cfba81a7
adrianstanislaus <18118036@telecom.stei.itb.ac.id>
2021-09-15 22:23:31 +0700

    naananan

--- main.go
+++ main.go
@@ -11,5 +11,7 @@ func main() {
 
 	fmt.Println("This is the main code, the one and only, the original code")
 	fmt.Println("ini tulisan setelah stash")
+	fmt.Println("This is the main code, the one and only, the original code")
+	fmt.Println("ini tulisan setelah stash")
 
 }
