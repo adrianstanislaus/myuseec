001ce34622aef4c36d59b4ae124260da314cf288
adrianstanislaus <18118036@telecom.stei.itb.ac.id>
2021-09-17 10:44:25 +0700

    empty

--- main.go
+++ main.go
@@ -1,16 +1,4 @@
 package main
 
 func main() {
-<<<<<<< HEAD
-	fmt.Println("This is the MAIN code")
-	fmt.Println("This is the DEVELOPMENT code")
-<<<<<<< HEAD
-	fmt.Println("This is the featureB code")
-=======
-  fmt.Println("This is the featureB code")
->>>>>>> d610c17a2324f5c043659f86b89122e3e38aa93b
-	fmt.Println("This is the featureA code")
-	fmt.Println("This is another featureA code")
-=======
->>>>>>> main
 }
