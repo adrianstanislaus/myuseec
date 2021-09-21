8a89e7a1ab0639e4eae2ea9c4f3ab43ea72115a7
adrianstanislaus <18118036@telecom.stei.itb.ac.id>
2021-09-21 23:25:09 +0700

    add users model

--- models/users/users_db.go
+++ models/users/users_db.go
@@ -10,8 +10,9 @@ type User struct {
 	Id                int            `gorm:"primaryKey" json:"id"`
 	FirstName         string         `json:"firstname"`
 	LastName          string         `json:"lastname"`
-	Username          string         `json:"username"`
-	Email             string         `json:"email"`
+	Username          string         `json:"username" gorm:"unique"`
+	Email             string         `json:"email" gorm:"unique"`
+	Password          string         `json:"password"`
 	Bio               string         `json:"bio"`
 	Profile_pic       string         `json:"profile_pic"`
 	Subscription_type string         `json:"subscription_type"`
---
+++ models/users/users_login.go
@@ -0,0 +1,6 @@
+package Users
+
+type UserLogin struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
